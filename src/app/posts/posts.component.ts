71318cd5d0f5c73e8d68f2d092aa222cc34a93f5
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2018-10-18 15:03:56 -0500

    handling expected and unexpected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -13,36 +13,63 @@ export class PostsComponent implements OnInit {
   constructor(private postsService: PostsService) { }
 
   ngOnInit() {
-    this.postsService.getPosts().subscribe(res => {
+    this.postsService.getPosts()
+    .subscribe(res => {
       console.log(res);
       this.posts = res;
+    }, 
+    error => {
+      alert('Ocurrió un error inesperado.');
+      console.log(error);      
     });
   }
 
   crearPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
-    this.postsService.servicioPost(input.value).subscribe(res => {
+    this.postsService.servicioPost(input.value)
+    .subscribe(
+      res => {
       //post['id']= res;
       this.posts.splice(0,0,post);
       console.log(res);
-    })
+    }, 
+    (error: Response) => {
+      if(error.status === 400) {
+        // this.form.setErrors(error.json()); en caso de tener un reactive form y mostrar un mensaje al lado de un input
+      } else {
+        alert('Ocurrió un error inesperado.');
+        console.log(error); 
+      }     
+    });
   }
 
   updatePost(post) {
-    this.postsService.update(post).subscribe(res => {
-      console.log(res);
-      
-    })
+    this.postsService.update(post)
+    .subscribe(res => {
+      console.log(res);      
+    }, 
+    error => {
+      alert('Ocurrió un error inesperado.');
+      console.log(error);      
+    });
   }
 
   deletePost(post) {
-    this.postsService.delete(post).subscribe(res => {
+    this.postsService.delete(post)
+    .subscribe(res => {
       console.log(res);
       let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-      
-    })
+      this.posts.splice(index, 1);      
+    }, 
+    (error: Response) => {
+      if(error.status === 404) {
+        alert('Este post ya ha sido borrado');
+      } else {
+        alert('Ocurrió un error inesperado.');
+      console.log(error); 
+      }           
+    });
   }
 
 }
