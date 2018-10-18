c88cd0754c27b81196f2db160381279d56e3c931
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2018-10-18 11:00:03 -0500

    se agregan los servicios post, patch y delete

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,6 +16,32 @@ export class PostsComponent implements OnInit {
     this.postsService.getPosts().subscribe(res => {
       console.log(res);
       this.posts = res;
+    });
+  }
+
+  crearPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+    this.postsService.servicioPost(input.value).subscribe(res => {
+      //post['id']= res;
+      this.posts.splice(0,0,post);
+      console.log(res);
+    })
+  }
+
+  updatePost(post) {
+    this.postsService.update(post).subscribe(res => {
+      console.log(res);
+      
+    })
+  }
+
+  deletePost(post) {
+    this.postsService.delete(post).subscribe(res => {
+      console.log(res);
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+      
     })
   }
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -8,7 +8,21 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
   getPosts() {
-    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get<any[]>(this.url);
+  }
+
+  servicioPost(post) {
+    return this.http.post(this.url, post);
+  }
+
+  update(post) {
+    return this.http.patch(this.url + '/' + post.id, {isRead: true});
+  }
+
+  delete(post) {
+    return this.http.delete(this.url + '/' + post.id);
   }
 }
