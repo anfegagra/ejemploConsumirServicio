07c0ad7f1d41e874f250940e0d6af68246d4d4fb
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2018-10-19 14:36:46 -0500

    se refactoriza el manejo de errores en posts.service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -15,29 +15,31 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
-  private url = 'https://sdfjsonplaceholder.typicode.com/posts';
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
   getPosts() {
     return this.http.get<any[]>(this.url);
   }
 
   servicioPost(post) {
-    return this.http.post(this.url, post).pipe(catchError((error: Response) => {
-      if(error.status === 400)
-        return throwError(new BadRequest(error));
-      return throwError(new AppError(error));
-    }));
+    return this.http.post(this.url, post).pipe(catchError(this.handleErrors));
   }
 
   update(post) {
-    return this.http.patch(this.url + '/' + post.id, {isRead: true});
+    return this.http.patch(this.url + '/' + post.id, {isRead: true}).pipe(catchError(this.handleErrors));
   }
 
   delete(post) {
-    return this.http.delete(this.url + '/' + post.id).pipe(catchError((error: Response) => {
-      if(error.status === 404)
-        return throwError(new NotFoundError());
-      return throwError(new AppError(error));
-    }));
+    return this.http.delete(this.url + '/' + post.id).pipe(catchError(this.handleErrors));
+  }
+
+  private handleErrors(error: Response) {
+    if(error.status === 404)
+      return throwError(new NotFoundError());
+
+    if(error.status === 400)
+      return throwError(new BadRequest(error));
+
+    return throwError(new AppError(error));
   }
 }
